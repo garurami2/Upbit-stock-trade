eff73f3b67e22bb038401af314e84ff4ccda6f96
garurami1 <garurami1@naver.com>
2025-05-29 19:07:37 +0900

    비트코인 조건 검색 변경후 결과 캡쳐

--- autotrade.py
+++ autotrade.py
@@ -9,6 +9,12 @@ import requests
 from openai import OpenAI
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 load_dotenv()
 
